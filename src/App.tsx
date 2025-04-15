a79b111155656daaf4d3631efd12ad603479083a
hakmoune <mehdi.hakmoune@gmail.com>
2025-04-15 23:22:31 +0200

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Journal from "./pages/Journal";
 import "./App.css";
+import RootLayout from "./pages/RootLayout";
 
 function App() {
   return (
-    <>
-      <h1 className="text-center">Hello World !</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route index element={<Navigate replace to="/journal" />} />
+          <Route path="/journal" element={<Journal />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
